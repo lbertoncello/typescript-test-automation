91a15193a0623a5afe5424a6633be51a3b28fa3c
lbertoncello <lucas_b_andrade@hotmail.com>
2021-09-02 19:05:47 -0300

    aula sobre criação de spec para messaging

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,43 @@
+import { Messaging } from './messaging';
+
+const createSut = () => {
+    return new Messaging()
+}
+
+describe('Persistency', () => {
+	afterEach(() => jest.clearAllMocks());
+
+	it('should return undefined', () => {
+		/*
+		 * --- SUT (System Under Test) ---
+		 * Convenção de nomenclatura para a instância da classe que
+		 * está sendo testada.
+		 */
+		const sut = createSut();
+
+		expect(sut.sendMessage('test')).toBeUndefined();
+	});
+
+	it('should call console.log once', () => {
+		const sut = createSut();
+		/*
+		 * --- Spy (um tipo de mock) ---
+		 * "Espiona" o objeto selecionada para verificar o comportamento tido por ele.
+		 * Ex.: verificar se um método foi chamado, quantas vezes foi chamado etc.
+		 */
+		const consoleSpy = jest.spyOn(console, 'log');
+
+		sut.sendMessage('test');
+
+		expect(consoleSpy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should call console.log with "Mensagem enviada:" and msg', () => {
+		const sut = createSut();
+		const consoleSpy = jest.spyOn(console, 'log');
+
+		sut.sendMessage('test');
+
+		expect(consoleSpy).toHaveBeenCalledWith('Mensagem enviada:', 'test');
+	});
+});
